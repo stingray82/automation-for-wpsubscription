d8d0dcdf45cac648c93b47f8d9f8b9f688f7235d
Stingray82 <11146912+stingray82@users.noreply.github.com>
2025-08-12 14:20:04 +0100

    Version 0.5.9 Release

--- automation-for-wpsubscription/automation-for-wpsubscription.php
+++ automation-for-wpsubscription/automation-for-wpsubscription.php
@@ -5,7 +5,7 @@
  * Tested up to:      6.8.2
  * Requires at least: 6.5
  * Requires PHP:      8.1
- * Version:           0.5.8
+ * Version:           0.5.9
  * Author:            reallyusefulplugins.com
  * Author URI:        https://reallyusefulplugins.com
  * License:           GPL2
@@ -23,7 +23,7 @@ if ( ! defined('ABSPATH') ) {
 require_once __DIR__ . '/includes/setup.php'; // Flowmattic Admin Test located in Automation
 
 // Define plugin constants
-define('rup_wpsco_automation_for__wpsubscription_VERSION', '0.5.8');
+define('rup_wpsco_automation_for__wpsubscription_VERSION', '0.5.9');
 define('rup_wpsco_automation_for__wpsubscription_SLUG', 'automation-for-wpsubscription'); // Replace with your unique slug if needed
 define('rup_wpsco_automation_for__wpsubscription_MAIN_FILE', __FILE__);
 define('rup_wpsco_automation_for__wpsubscription_DIR', plugin_dir_path(__FILE__));
